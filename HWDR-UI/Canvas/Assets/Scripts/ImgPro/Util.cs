ce353ef98a8db054cd59d8dc46134e3803d9afea
mnokno <prv.nowacki@gmail.com>
2023-04-22 23:31:25 +0100

    Created a skeleton for image utilities

---
+++ HWDR-UI/Canvas/Assets/Scripts/ImgPro/Util.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace ImgPro
+{
+    public static class Util
+    {
+        public static float[,] Center(float[,] img)
+        {
+            return null;
+        }
+
+        public static float[,] Blure(float[,] img)
+        {
+            return null;
+        }
+
+        public static float[,] ResizeDigit(float[,] img)
+        {
+            return null;
+        }
+
+        public static Texture2D Resize(Texture2D texture, int res)
+        {
+            return null;
+        }
+
+        public static float[,] TextureToRaw(Texture2D texture)
+        {
+            return null;
+        }
+
+        public static Texture2D RawToTexture(float[,] img)
+        {
+            return null;
+        }
+    }
+}
