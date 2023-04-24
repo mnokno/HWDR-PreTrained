9908c13f93afb2c1db52304b23b71b8337756afb
mnokno <prv.nowacki@gmail.com>
2023-04-24 16:11:40 +0100

    Update ImagePreview.cs

--- HWDR-UI/Canvas/Assets/Scripts/ImagePreview.cs
+++ HWDR-UI/Canvas/Assets/Scripts/ImagePreview.cs
@@ -1,51 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Shows a preview of what the neural network will review as an input
+/// </summary>
 public class ImagePreview : MonoBehaviour
 {
-    private RectTransform rectTransform;
+    /// <summary>
+    /// Image used to display the texture
+    /// </summary>
     private Image image;
+    /// <summary>
+    /// Texture displayed on the image
+    /// </summary>
     private Texture2D texture;
-    private Texture2D previewOf;
 
-    public void Setup()
-    {
-        // Create a new texture to use as the canvas
-        texture = new Texture2D(28, 28, TextureFormat.RGBA32, false);
-        texture.filterMode = FilterMode.Point;
-        texture.wrapMode = TextureWrapMode.Clamp;
-        Color32[] pixels = new Color32[texture.width * texture.height];
-        for (int i = 0; i < pixels.Length; i++)
-        {
-            pixels[i] = Color.white;
-        }
-        texture.SetPixels32(pixels);
-        texture.Apply();
-        image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-    }
 
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    private void Start()
     {
-        rectTransform = GetComponent<RectTransform>();
         image = GetComponent<Image>();
-        previewOf = FindObjectOfType<DrawingCanvas>().GetTexture();
-        Setup();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float[,] data = ImgPro.Util.ResizeDigit(ImgPro.Util.Centre(ImgPro.Util.TextureToRaw(previewOf)));
-     
-        if (data.Length > 0)
-        {
-            Texture2D newTexture = ImgPro.Util.RawToTexture(data);
-            ImgPro.Util.Resize(newTexture, 28);
-            texture.SetPixels(newTexture.GetPixels());
-            texture.Apply();
-        }
+        texture = FindObjectOfType<DrawingCanvas>().GetTexture();
+        image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
     }
 }
